006ff84bad1be98c77db26d03488b2d2e2d3447b
DNAnh01 <donguyenanhgithub@gmail.com>
2023-12-22 13:42:02 +0700

    #3 install sass and config prettierrc

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import "./App.css";
+import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <div>My Calendar</div>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div className="App">
+            <div>My Calendar</div>
+        </div>
+    );
+};
 
 export default App;
