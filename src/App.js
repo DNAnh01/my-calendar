fcf71ccb1fb8827affc4038a4cf9901331f42578
DNAnh01 <donguyenanhgithub@gmail.com>
2023-12-22 20:27:26 +0700

    release v1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { useState } from 'react';
 import './App.css';
 
+import { format } from 'date-fns';
+import Calendar from '~/components/Calendar';
+
 const App = () => {
+    const [currentDate, setCurrentDate] = useState(new Date('2023-01-01'));
+
+    const handleSetToday = () => {
+        setCurrentDate(new Date());
+    };
     return (
-        <div className="App">
-            <div>My Calendar</div>
+        <div className={`App align-items-center d-flex flex-column mt-5 gap-4`}>
+            <div>Selected day: {format(currentDate, 'dd LLL yyyy')}</div>
+            <button onClick={handleSetToday}>Today</button>
+            <Calendar value={currentDate} onChange={setCurrentDate} />
         </div>
     );
 };
---
+++ src/components/Calendar/Calendar.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { startOfMonth, endOfMonth, differenceInDays, format, sub, add, setDate } from 'date-fns';
+
+import classNames from 'classnames/bind';
+import styles from './Calendar.module.scss';
+
+import Cell from '~/components/Cell';
+
+const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+const cx = classNames.bind(styles);
+const Calendar = ({ value, onChange }) => {
+    const startDate = startOfMonth(value);
+    const endDate = endOfMonth(value);
+    const numDays = differenceInDays(endDate, startDate) + 1;
+
+    const prefixDays = startDate.getDay();
+    const suffixDays = 6 - endDate.getDay();
+
+    const prevMonth = () => onChange && onChange(sub(value, { months: 1 }));
+    const nextMonth = () => onChange && onChange(add(value, { months: 1 }));
+
+    const prevYear = () => onChange && onChange(sub(value, { years: 1 }));
+    const nextYear = () => onChange && onChange(add(value, { years: 1 }));
+
+    const handleClickDate = (index) => {
+        const date = setDate(value, index);
+        onChange && onChange(date);
+    };
+
+    return (
+        <div className={cx('container')}>
+            <div className={cx('content')}>
+                <Cell onClick={prevYear}>{'<<'}</Cell>
+                <Cell onClick={prevMonth}>{'<'}</Cell>
+                <Cell className={cx('title')}>{format(value, 'LLLL yyyy')}</Cell>
+                <Cell onClick={nextMonth}>{'>'}</Cell>
+                <Cell onClick={nextYear}>{'>>'}</Cell>
+
+                {daysOfWeek.map((day) => (
+                    <Cell key={day} className={cx('day-cell')}>
+                        {day}
+                    </Cell>
+                ))}
+
+                {Array.from({ length: prefixDays }).map((_, index) => (
+                    <Cell key={index} />
+                ))}
+
+                {Array.from({ length: numDays }).map((_, index) => {
+                    const date = index + 1;
+                    const isCurrentDate = date === value.getDate();
+                    return (
+                        <Cell key={index} active={isCurrentDate} onClick={() => handleClickDate(date)}>
+                            {date}
+                        </Cell>
+                    );
+                })}
+
+                {Array.from({ length: suffixDays }).map((_, index) => (
+                    <Cell key={index} />
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Calendar;
